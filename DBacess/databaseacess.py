8b5453a0ae47da56ca90fa301c10b8c74efca749
Pablo Vinícios <128994831+PabloVini28@users.noreply.github.com>
2024-05-14 14:00:39 -0300

    Update databaseacess.py

--- DBacess/databaseacess.py
+++ DBacess/databaseacess.py
@@ -1,11 +1,11 @@
-from tracemalloc import Statistic
 import mysql.connector
 import statistics
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 
-
-con = mysql.connector.connect(host='localhost',database='dadoshospitalares',user='root',password='')
+con = mysql.connector.connect(host='localhost', database='dadoshospitalares', user='root', password='')
 cursor = con.cursor()
 
 meses = ["janeiro", "fevereiro", "marco", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"]
@@ -20,13 +20,25 @@ X = np.array(dados[:-1]).reshape(-1, 1)
 y = np.array(dados[1:]).reshape(-1, 1) 
 
 model = LinearRegression()
-
 model.fit(X, y)
 
-previsao_proximo_ano = model.predict([[dados[-1]]])
+previsao_proximo_ano = model.predict(np.array([dados[-1]]).reshape(-1, 1))
 
+
+model = Sequential()
+model.add(Dense(64, activation='relu', input_shape=(1,)))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(1))
+
+# Compilar o modelo
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Treinar o modelo
+model.fit(X, y, epochs=100, batch_size=1, verbose=1)
+
+# Fazer a previsão para dezembro do próximo ano
+previsao_proximo_ano = model.predict(np.array([dados[-1]]).reshape(-1, 1))
 print("Previsão de nascidos vivos para dezembro de 2024:", previsao_proximo_ano[0][0])
-
 
 cursor.execute("select * from janeiro;")
 Janeiro = cursor.fetchall();
@@ -52,7 +64,6 @@ cursor.execute("select * from novembro;")
 Novembro = cursor.fetchall();
 cursor.execute("select * from dezembro;")
 Dezembro = cursor.fetchall();
-
 
 dadosn = ["nascidos vivos","nascidos masculinos","nascidos femininos","nascidos gemeos","nascidos de cesaria","nascidos de vaginal","boa vitalidade","RN_baixo peso","RN_peso adequado","RN_pre_maturo"]
 for  i in range (1,10) :
